c5ac7a54dc5b6d17a953798901e074189575b16b
Hugh Emberson <hugh.emberson@gmail.com>
2025-12-01 20:54:21 -0700

    Run `conditional_comp` example with failpoints enabled and disabled.

--- run_tests.sh
+++ run_tests.sh
@@ -5,8 +5,12 @@ set -e
 echo "Running all tests"
 cargo test -- --test-threads=1
 
-echo "Running example: conditional_comp"
+echo "Running example: conditional_comp (enabled)"
 cargo run --example conditional_comp
+
+echo "Running example: conditional_comp (disabled)"
+cargo run --example conditional_comp --no-default-features
+
 
 echo "Running example: failpoint"
 cargo run --example failpoint
